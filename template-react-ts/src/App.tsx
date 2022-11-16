766d2d533ccc251d71546486b1da8c65266f5bb8
shiqi <shiqi@indexed.cn>
2022-11-16 15:02:46 +0800

    feat: template-react-ts add request & react-query

--- template-react-ts/src/App.tsx
+++ template-react-ts/src/App.tsx
@@ -1,13 +1,20 @@
 import { Button } from 'antd'
+import { useQuery } from '@tanstack/react-query'
 import './App.css'
 
 function App() {
+
+  const { data } = useQuery({
+    queryKey: ['test'],
+    queryFn: () => Promise.resolve(5)
+  })
 
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <h2>useQuery response {data}</h2>
       <Button type='primary'>primary</Button>
     </div>
   )
